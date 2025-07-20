ff12d8374b3c96964262026fa77cace8e4da8ce1
David McCullough <dmccullough488@gmail.com>
2025-07-20 00:32:40 -0500

    added fun to DOM radio input and add css class

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-emotionRadios.addEventListener('change', function (e) {
-  console.log(e.target.id)
-})
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+
+function highlightCheckedOption(e) {
+  document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
 
 function getEmotionsArray(cats) {
   const emotionsArray = []
