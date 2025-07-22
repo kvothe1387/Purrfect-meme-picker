2f70807d2905bac584c93212c60c507223dd83d0
David McCullough <dmccullough488@gmail.com>
2025-07-21 22:56:02 -0500

    DOM gifs only checkbox and set const in getMC[] to boolean value true if box is checked

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
@@ -17,6 +18,10 @@ function highlightCheckedOption(e) {
 }
 
 function getMatchingCatsArray() {
+  const isGif = gifsOnlyOption.checked
+  console.log(isGif)
+
+
   if (document.querySelector('input[type="radio"]:checked')) {
     const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
     console.log(selectedEmotion)
