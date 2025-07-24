51788a19fabe34e64f418a621673148c0cbf9766
David McCullough <dmccullough488@gmail.com>
2025-07-24 00:46:46 -0500

    complete the renderCat & closeModal fun

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ import { catsData } from "./data.js"
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeCloseBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 getImageBtn.addEventListener('click', renderCat)
 
+memeCloseBtn.addEventListener('click', closeModal)
+
+function closeModal() {
+  memeModal.style.display = "none"
+}
 
 function highlightCheckedOption(e) {
   const radios = document.getElementsByClassName('radio')
@@ -37,12 +45,22 @@ function getSingleCatObject() {
   const catsArray = getMatchingCatsArray()
 
   if (catsArray.length === 1) {
-    console.log(catsArray[0])
+    return catsArray[0]
+  } else {
+    const randomIndex = Math.floor(Math.random() * catsArray.length)
+    return catsArray[randomIndex]
   }
 }
 
 function renderCat() {
-
+  const catObject = getSingleCatObject()
+  memeModalInner.innerHTML = `
+    <img
+      class="cat-img"
+      src="./imgaes/${catObject.image}"
+      alt="${catObject.alt}"
+    >`
+  memeModal.style.display = "flex"
 }
 
 function getEmotionsArray(cats) {
